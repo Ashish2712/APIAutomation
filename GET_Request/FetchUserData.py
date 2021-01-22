6113bcb1f5a9766e5e925223ffd72414dde02d1d
Ashish singh kushwah <akushwah206@gmail.com>
2021-01-22 21:20:54 +0530

    Update FetchUserData.py
    Added post method

--- GET_Request/FetchUserData.py
+++ GET_Request/FetchUserData.py
@@ -1,5 +1,15 @@
 # API URL
-
+import requests
 
 url = "https://reqres.in//api/users?page=2"
+
+
+def post(url):
+  """
+  post data on the url
+  """
+  data = {name: 'github'}
+  
+  response = requests.post(url, data)
+  return response.status_code
 
